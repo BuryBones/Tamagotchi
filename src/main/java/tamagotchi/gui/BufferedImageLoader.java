7810dfdc6ebb0e8f20aa9200401d2b05b626cd08
Bury_Bones <derkach.a.d@gmail.com>
2021-04-26 18:37:23 +0400

    Add mnemonics and button icons, add version field to Constants

--- src/main/java/tamagotchi/gui/BufferedImageLoader.java
+++ src/main/java/tamagotchi/gui/BufferedImageLoader.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 
 public class BufferedImageLoader {
 
-  private BufferedImage image;
+    private BufferedImage image;
 
-  public BufferedImage loadImage(String path) {
-    try {
-      image = ImageIO.read(getClass().getClassLoader().getResource(path));
-      //            image = ImageIO.read(new File(path));
-    } catch (IOException e) {
-      e.printStackTrace();
+    public BufferedImage loadImage(String path) {
+        try {
+            image = ImageIO.read(getClass().getClassLoader().getResource(path));
+            //            image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
-    return image;
-  }
 }
